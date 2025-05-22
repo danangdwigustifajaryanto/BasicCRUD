8ad518e9aaf481d4895d1c1ea2b4ff3fab3163f1
Danang Dwi Gusti Fajar Yanto <57741344+danangdwigustifajaryanto@users.noreply.github.com>
2025-05-22 08:49:55 +0700

    Add files via upload

--- kasircrud/config/koneksi.php
+++ kasircrud/config/koneksi.php
@@ -37,3 +37,31 @@ CREATE TABLE detail_transaksi (
     FOREIGN KEY (id_transaksi) REFERENCES transaksi(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (id_barang) REFERENCES barang(id) ON DELETE CASCADE ON UPDATE CASCADE
 ); -->
+
+<!-- contoh data dummy
+-- Insert ke tabel barang
+INSERT INTO barang (id, nama, harga, stok) VALUES
+(1, 'Pensil 2B', 2000, 100),
+(2, 'Buku Tulis', 5000, 50),
+(3, 'Penghapus', 1500, 75),
+(4, 'Penggaris 30cm', 4000, 40),
+(5, 'Bolpoin Biru', 2500, 60);
+
+-- Insert ke tabel transaksi
+INSERT INTO transaksi (id, tanggal, total) VALUES
+(1, '2025-05-14 08:30:00', 9500),
+(2, '2025-05-14 10:15:00', 13500),
+(3, '2025-05-14 14:45:00', 6500);
+
+-- Insert ke tabel detail_transaksi
+INSERT INTO detail_transaksi (id, id_transaksi, id_barang, jumlah, subtotal) VALUES
+(1, 1, 1, 2, 4000),       -- 2x Pensil = 4000
+(2, 1, 3, 1, 1500),       -- 1x Penghapus = 1500
+(3, 1, 5, 1, 2500),       -- 1x Bolpoin = 2500
+
+(4, 2, 2, 2, 10000),      -- 2x Buku = 10000
+(5, 2, 3, 1, 1500),       -- 1x Penghapus = 1500
+(6, 2, 1, 1, 2000),       -- 1x Pensil = 2000
+
+(7, 3, 4, 1, 4000),       -- 1x Penggaris = 4000
+(8, 3, 5, 1, 2500);       -- 1x Bolpoin = 2500 -->
